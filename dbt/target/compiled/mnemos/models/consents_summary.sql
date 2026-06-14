0d706759f0da974db0edd92f2422f03ab2ed1855
Ruben <dcrubben25@gmail.com>
2026-06-14 22:56:02 +0200

    chore: gitignore dbt generated files

--- dbt/target/compiled/mnemos/models/consents_summary.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  COUNT(*) as total_consentimientos,
-  COUNT(CASE WHEN status = 'obtained' THEN 1 END) as aprobados,
-  COUNT(CASE WHEN status = 'denied' THEN 1 END) as rechazados,
-  COUNT(CASE WHEN status = 'pending' THEN 1 END) as pendientes,
-  DATE_TRUNC('month', created_at) as mes
-FROM "mnemos"."public"."consents"
-GROUP BY DATE_TRUNC('month', created_at)
